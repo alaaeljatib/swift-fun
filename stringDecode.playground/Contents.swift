ebb84bbef0209354cf42ecd6f45ec99a6bf4bcc4
Alaa Eljatib <alaa.eljatib@gmail.com>
2019-02-06 13:46:50 -0500

    Add DecodeString

--- stringDecode.playground/Contents.swift
+++ stringDecode.playground/Contents.swift
@@ -1,4 +1,3 @@
-import UIKit
 
 class Solution {
     func decodeString(_ s: String) -> String {
@@ -25,7 +24,7 @@ class Solution {
             let numOfDigits = Array(temp).count
             
             for  i in 0..<numOfDigits {
-                count += Int("\(Array(temp)[i])") ?? 0 * Int(truncating:NSDecimalNumber(decimal:pow(10,numOfDigits - 1 - i)))
+                count = count*10 + Int("\(Array(temp)[i])")!
             }
             
             temp = ""
@@ -33,4 +32,51 @@ class Solution {
         }
         return result
     }
+    
+    static func decodeStringWithRecursiveLogic(_ s: String) -> String {
+        var indx = 0
+        return decodeStringRec(s, &indx)
+    }
+    
+    static private func decodeStringRec(_ s: String, _ indx : inout Int) -> String {
+        if (s.count == 0 ) {
+            return ""
+        }
+        
+         var result = ""
+         var number = 0
+        
+        while (indx < s.count && Array(s)[indx] != "]") {
+           
+            let currentChar = Array(s)[indx]
+            
+    
+            
+            if "0"..."9" ~= currentChar { // is number
+                number = number * 10 + Int(String(currentChar))!
+                indx += 1
+        
+            }
+            
+            if "a"..."z" ~= currentChar || "A"..."Z" ~= currentChar { // is letter
+                result += String(currentChar)
+                indx += 1
+            }
+            
+            if currentChar == "[" {
+                indx += 1
+                let innerString = decodeStringRec(s, &indx)
+                
+                while (number > 0) {
+                    result += innerString
+                    number -= 1
+                }
+            }
+        }
+ 
+        indx += 1;
+        return result
+    }
 }
+
+ Solution.decodeStringWithRecursiveLogic("3[a]2[bc]")
