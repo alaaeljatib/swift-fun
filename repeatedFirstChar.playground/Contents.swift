e1619f335ba0bc8fdb6f5abc1ad31571e3060b0c
Alaa Eljatib <alaa.eljatib@gmail.com>
2019-03-26 12:28:56 -0400

    Add repeatedString

--- repeatedFirstChar.playground/Contents.swift
+++ repeatedFirstChar.playground/Contents.swift
@@ -2,36 +2,24 @@ import Foundation
 
 // Complete the repeatedString function below.
 func repeatedString(s: String, n: Int) -> Int {
-    guard s.count > 0, n >= s.count else { return 0}
+    guard s.count > 0 else { return 0}
     
-    if s.count == 1 {
-        return n
-    }
+    //    if s.count == 1 {
+    //        if s == "a" {
+    //            return n
+    //        }
+    //        return 0
+    //    }
     
+    let numOfRepeatedFirstChars = s.filter{$0 == "a"}.count
     
-    var numOfRepeatedFirstChars = 0
+    let modValue = n % s.count
+    let indexValue = modValue > 0 ? modValue - 1 : 0
+    let extraRepeated = s[...s.index(s.startIndex, offsetBy: indexValue)].filter{$0 == "a"}.count
     
-    for c in s {
-        if c == s[s.startIndex] {
-            numOfRepeatedFirstChars = numOfRepeatedFirstChars + 1
-        }
-    }
-    
-    if numOfRepeatedFirstChars == 1 {
-        return n
-    } else {
-        let modValue = n % s.count
-        
-        var extraRepeated = 0
-        for i in 1...modValue {
-            if s[s.startIndex] == s[s.index(s.startIndex, offsetBy: i)] {
-                extraRepeated  = extraRepeated + 1
-            }
-        }
-        
-        return (numOfRepeatedFirstChars * (n / s.count)) + extraRepeated
-    }
+    return (numOfRepeatedFirstChars * (n / s.count)) + extraRepeated
 }
 
-repeatedString(s: "gfcaaaecbg", n: 547602)
+
+repeatedString(s: "kmretasscityylpdhuwjirnqimlkcgxubxmsxpypgzxtenweirknjtasxtvxemtwxuarabssvqdnktqadhyktagjxoanknhgilnm", n: 736778906400)
 
